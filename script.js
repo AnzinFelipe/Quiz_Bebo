949a6bd5575a5e11728024199e046505a63ef2e5
AnzinFelipe <ian.felipe.drunen@gmail.com>
2025-06-17 15:58:17 -0300

    Adicionei as 10 perguntas no script

---
+++ script.js
@@ -0,0 +1,92 @@
+const perguntas = [
+  {
+    pergunta: "Que nome de pokemon NUNCA saiu da sua boca?",
+    respostas: [
+      { id: 1, texto: "Togedemaru", correto: false },
+      { id: 2, texto: "Clodsire", correto: false },
+      { id: 3, texto: "Sawsbuck", correto: true },
+      { id: 4, texto: "Stakataka", correto: false },
+    ],
+  },
+  {
+    pergunta: "O que que VOCÊ é?",
+    respostas: [
+      { id: 1, texto: "Um baiacu", correto: true },
+      { id: 2, texto: "Um tipo de fecho-éclair", correto: false },
+      { id: 3, texto: "Um macaco do Bloons TD6", correto: false },
+      { id: 4, texto: "Um porta-moedas", correto: false },
+    ],
+  },
+  {
+    pergunta: "Quando é o MEU aniversário? Seja sincero",
+    respostas: [
+      { id: 1, texto: "27 de Outubro", correto: false },
+      { id: 2, texto: "29 de Novembro", correto: false },
+      { id: 3, texto: "26 de Novembro", correto: false },
+      { id: 4, texto: "Eu nunca lembro", correto: true },
+    ],
+  },
+  {
+    pergunta: "Quantos pseudônimos VOCÊ tem?",
+    respostas: [
+      { id: 1, texto: "2", correto: false },
+      { id: 2, texto: "Infinitos", correto: true },
+      { id: 3, texto: "20 e 10", correto: false },
+      { id: 4, texto: "Nenhum", correto: false },
+    ],
+  },
+  {
+    pergunta: "A crase nunca ocorre do The Jeanus and Beunbus é uma parodia de que música?",
+    respostas: [
+      { id: 1, texto: "Fico Assim Sem Você", correto: false },
+      { id: 2, texto: "Beautiful Girls", correto: true },
+      { id: 3, texto: "Die Hard", correto: false },
+      { id: 4, texto: "The Only Thing I Know For Real", correto: false },
+    ],
+  },
+  {
+    pergunta: "Quem VOCÊ era na série de Amor Doce do Dancing Peras?",
+    respostas: [
+        { id: 1, texto: "Armin", correto: false },
+        { id: 2, texto: "Castiel", correto: false },
+        { id: 3, texto: "Jade", correto: true },
+        { id: 4, texto: "Lysandre", correto: false },
+      ],
+  },
+  {
+    pergunta: 'O vídeo "Video tutorial para CAIO MENEZES" é um tutorial ensinando como...',
+    respostas: [
+        { id: 1, texto: "Baixar Deltarune chapter 3", correto: false },
+        { id: 2, texto: "Baixar Minecraft", correto: false },
+        { id: 3, texto: "Baixar o TLauncher", correto: false },
+        { id: 4, texto: "Baixar Undertale 100% gratuito real sem vírus 2023 e com salve", correto: true },
+      ],
+  },
+  {
+    pergunta: "Faça essa soma simples: ovo + ovo = ?",
+    respostas: [
+        { id: 1, texto: "janta", correto: false },
+        { id: 2, texto: "Oovvoo", correto: false },
+        { id: 3, texto: "2 ovos", correto: false },
+        { id: 4, texto: "Queijo", correto: true },
+      ],
+  },
+  {
+    pergunta: "Como Heitorzinho voltou ao passado pela primeira vez?",
+    respostas: [
+        { id: 1, texto: "Ele fez um desejo", correto: false },
+        { id: 2, texto: "Ele caiu nos trilhos do metrô", correto: false },
+        { id: 3, texto: "Ele comeu um cuscuz paulista", correto: true },
+        { id: 4, texto: "Ele apertou a mão de Jotave 🥚", correto: false },
+      ],
+  },
+  {
+    pergunta: "Gachachulimoneropalamano...",
+    respostas: [
+        { id: 1, texto: "Minino", correto: true },
+        { id: 2, texto: "Pipinhos", correto: false },
+        { id: 3, texto: "Minhoquios", correto: false },
+        { id: 4, texto: "Bucodinho", correto: false },
+      ],
+  }
+];
